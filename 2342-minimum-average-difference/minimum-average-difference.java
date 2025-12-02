670613daad06e09ff11ab4fe3b0e8a972cab552c
Tanuja0318 <165898802+Tanuja0318@users.noreply.github.com>
2025-12-02 15:14:34 +0530

    Time: 7 ms (25.87%) | Memory: 99.7 MB (11.44%) - LeetSync

---
+++ 2342-minimum-average-difference/minimum-average-difference.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int minimumAverageDifference(int[] nums) {
+        int n=nums.length;
+        long[] left = new long[n];
+        long[] right = new long[n];
+        left[0] = nums[0];
+        for (int i = 1; i < n; i++) {
+            left[i] = left[i - 1] + nums[i];
+        }
+        right[n - 1] = nums[n - 1];
+        for (int i = n - 2; i >= 0; i--) {
+            right[i] = right[i + 1] + nums[i];
+        }
+        long minDiff = Long.MAX_VALUE;
+        int ansIndex = 0;
+        for (int i = 0; i < n; i++) {
+            long leftSum = left[i];
+            long rightSum = (i == n - 1) ? 0 : right[i + 1];
+            long leftAvg = leftSum / (i + 1);
+            long rightAvg = (i == n - 1) ? 0 : rightSum / (n - i - 1);
+            long diff = Math.abs(leftAvg - rightAvg);
+            if (diff < minDiff) {
+                minDiff = diff;
+                ansIndex = i;
+            }
+        }
+        return ansIndex;
+    }
+}
